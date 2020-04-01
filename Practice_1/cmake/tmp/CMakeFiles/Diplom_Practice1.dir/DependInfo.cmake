385a6ebd65945630d34caede0dbe3d7b2cf9e4d1
e1nfalt <sergio.petrovx@yandex.ru>
2020-04-01 22:31:42 +0300

    some progress?

---
+++ Practice_1/cmake/tmp/CMakeFiles/Diplom_Practice1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/seryogas/Desktop/src/Diplom/Practice_1/cmake/main.cpp" "/Users/seryogas/Desktop/src/Diplom/Practice_1/cmake/tmp/CMakeFiles/Diplom_Practice1.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
